972d05c40c3be8b62f3e9704f4f929baebe0fffb
sorebear <sorenbaird@gmail.com>
2017-11-01 09:21:53 -0700

    Removed All Console Logs

--- src/reducers/background_reducer.js
+++ src/reducers/background_reducer.js
@@ -3,7 +3,6 @@ import types from '../actions/types';
 const DEFAULT_STATE = { background : Math.floor(Math.random() * 16) }
 
 export default (state = DEFAULT_STATE, action) => {
-    console.log("Current Background: ", action.payload)
     switch(action.type) {
         case types.CHANGE_BACKGROUND:
             return {...state, background: action.payload}
